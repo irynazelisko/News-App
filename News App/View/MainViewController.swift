b0d075c4bf8d49f8b131dbf2a67cd526426f049b
Iryna <irazelisko@gmail.com>
2023-03-15 14:09:36 +0200

    add favourite button, and swipe actions

--- News App/View/MainViewController.swift
+++ News App/View/MainViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 final class MainViewController: UIViewController {
     
-     var cellViewModel = TableCellViewModel()
+    var cellViewModel = TableCellViewModel()
     
     @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
@@ -17,7 +17,6 @@ final class MainViewController: UIViewController {
         tableView.delegate = self
         tableView.dataSource = self
         self.tableView.register(UINib(nibName: "NewsTableViewCell", bundle: nil), forCellReuseIdentifier: "NewsTableViewCell")
-
     }
 }
 
@@ -29,11 +28,11 @@ extension MainViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "NewsTableViewCell", for: indexPath)
+        let article = cellViewModel.newsArray[indexPath.row]
         
         guard let articleCell = cell as? NewsTableViewCell else {
             return cell
         }
-        let article = cellViewModel.newsArray[indexPath.row]
         
         articleCell.title.text = article.title
         articleCell.author.text = article.author
@@ -46,17 +45,45 @@ extension MainViewController: UITableViewDelegate, UITableViewDataSource {
                 }
             }
         }
-    
+        
         return articleCell
-    
+        
     }
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-            let newCell = cellViewModel.newsArray[indexPath.row]
-            let urlToImage = newCell.urlToImage
-            
-            let webViewController = WebViewController(urlString: urlToImage)
-            self.navigationController?.pushViewController(webViewController, animated: true)
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let article = cellViewModel.newsArray[indexPath.row]
+        let cell = tableView.cellForRow(at: indexPath) as! NewsTableViewCell
+        
+        
+        // Створюємо favorite action
+        let favoriteAction = UIContextualAction(style: .normal, title: "Favorite") { (action, view, completion) in
+            self.cellViewModel.addToFavorites(with: article.id, for: cell.favoriteButton)
+            print("Added to favorites")
+            completion(true)
         }
-
-   
+        favoriteAction.backgroundColor = .lightGray
+        
+        
+        // Створюємо unfavorite action
+        let unfavoriteAction = UIContextualAction(style: .destructive, title: "Unfavorite") { (action, view, completion) in
+            self.cellViewModel.removeFromFavorites(with: article.id, for: cell.favoriteButton)
+            print("Removed from favorites")
+            completion(true)
+        }
+        
+        
+        // Дії які виконуються під час проведення пальцем по рядках таблиці
+        let configuration = UISwipeActionsConfiguration(actions: [unfavoriteAction, favoriteAction])
+        return configuration
+    }
+    
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let newCell = cellViewModel.newsArray[indexPath.row]
+        let urlToImage = newCell.urlToImage
+        
+        let webViewController = WebViewController(urlString: urlToImage)
+        self.navigationController?.pushViewController(webViewController, animated: true)
+    }
+    
+    
 }
--- News App/View/NewsTableViewCell.swift
+++ News App/View/NewsTableViewCell.swift
@@ -8,21 +8,27 @@
 import UIKit
 
 final class NewsTableViewCell: UITableViewCell {
-
+    
     @IBOutlet weak  var urlToImage: UIImageView!
     @IBOutlet weak  var title: UILabel!
     @IBOutlet weak  var author: UILabel!
     @IBOutlet weak  var source: UILabel!
-  
+    @IBOutlet weak var favoriteButton: UIButton!
+    
+    
+    override func prepareForReuse() {
+        urlToImage.image = nil
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
     
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
     
