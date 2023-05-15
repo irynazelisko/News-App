b9445b6454b1d9eeb6835df254a87ef49e5fce27
Iryna <irazelisko@gmail.com>
2023-05-15 11:35:02 +0300

    schema version: 4

--- News App/AppDelegate.swift
+++ News App/AppDelegate.swift
@@ -16,10 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         let config = Realm.Configuration(
-            schemaVersion: 2,
+            schemaVersion: 4,
             migrationBlock: { migration, oldSchemaVersion in
                 
-                if (oldSchemaVersion < 2) {
+                if (oldSchemaVersion < 4) {
                 }
             })
         Realm.Configuration.defaultConfiguration = config
